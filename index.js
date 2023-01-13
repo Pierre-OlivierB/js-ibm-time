c64931a5a7f2cbd9ea24137bfd82d204e012456b
unknown <baudinopierreolivier@gmail.com>
2023-01-13 16:51:58 +0100

    test commit

--- index.js
+++ index.js
@@ -77,3 +77,68 @@ if (isNaN(yAge) || isNaN(mAge) || isNaN(dAge)) {
     // console.log(dAct);
   }
 }
+//----------------------------------------------
+//Clock update
+// document.
+// const h = document.getElementById("houre");
+// const m = document.getElementById("minutes");
+// const s = document.getElementById("secondes");
+
+// console.log(h, m, s);
+//---------------------------------------
+//Parité
+// let choice = prompt("Veuillez rentrer un nombre! Est-il pair?");
+function isEven(e) {
+  if (e % 2 === 1) {
+    console.log("Le nombre est impair.");
+  } else {
+    console.log("Le nombre est pair.");
+  }
+}
+// isEven(choice);
+//-------------------------------------------
+//Remise if +150 =>return 1%
+// let price = prompt("Taper le montant à payer : ");
+let price = 151;
+if (price > 150) {
+  //   console.log(price * 0.01);
+  price = price - price * 0.01;
+}
+// console.log(price);
+//----------------------------------------
+//Factoriel
+let input = prompt("Entrez un chiffre pour connaitre son factoriel");
+// let input = 4;
+let inFact = 1;
+
+function fact(e) {
+  //   for (let i = e; i > 0; i--) {
+  //     inFact *= i;
+  //   }
+  //   let flag = 1;
+  if (e == 0) {
+    inFact = 1;
+  }
+  for (let i = e; i > 0; i--) {
+    let result;
+    inFact *= i;
+    for (let j = i - 1; j > 0; j--) {
+      result += inFact;
+    }
+  }
+  //   while (0 < e) {
+  //     let add = e;
+  //     while (0 < e - 1) {
+  //       inFact *= e;
+  //       e--;
+  //       add += inFact;
+  //     }
+  //     console.log(add);
+  //     e--;
+  //   }
+
+  return inFact;
+}
+fact(input);
+console.log(inFact);
+//
