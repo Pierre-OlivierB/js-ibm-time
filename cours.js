0fae215b0bd044177e6c7268782587dcc3bf74c7
unknown <baudinopierreolivier@gmail.com>
2023-01-17 14:47:27 +0100

    while / do while

--- cours.js
+++ cours.js
@@ -1,3 +1,14 @@
 // console.log("test");
 //-------------------------------
-//Else if
+//while / do while
+let bol = 10;
+do {
+  console.log("je mange");
+  bol--;
+} while (bol > 6);
+
+bol = 10;
+while (bol > 6) {
+  console.log("je bois");
+  bol--;
+}
