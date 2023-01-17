dbe71aa0b00602158e6536c3c63fd127b10b7f79
unknown <baudinopierreolivier@gmail.com>
2023-01-17 15:25:51 +0100

    switch test

--- cours.js
+++ cours.js
@@ -3,12 +3,34 @@
 //while / do while
 let bol = 10;
 do {
-  console.log("je mange");
+  // console.log("je mange");
   bol--;
 } while (bol > 6);
 
 bol = 10;
 while (bol > 6) {
-  console.log("je bois");
+  // console.log("je bois");
   bol--;
 }
+//switch
+var test = "10";
+// var test = 10;
+switch (test) {
+  case 10:
+    console.log("c'est sans les quotes");
+    break;
+  case "10":
+    console.log("c'est bien avec les quotes");
+    break;
+  default:
+    console.log("test");
+    break;
+}
+// switch (key) {
+//   case value:
+
+//     break;
+
+//   default:
+//     break;
+// }
