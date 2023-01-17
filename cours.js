019286ecaa9be6286e7867c2bcec4a91968cc775
unknown <baudinopierreolivier@gmail.com>
2023-01-17 16:57:04 +0100

    quizz responses

--- cours.js
+++ cours.js
@@ -26,6 +26,79 @@ switch (test) {
     console.log("test");
     break;
 }
+
+var lesMois_arr = [
+  "",
+  "Janvier",
+  "Février",
+  "Mars",
+  "Avril",
+  "Mai",
+  "Juin",
+  "Juillet",
+  "Août",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Décembre",
+];
+var leMois = 2;
+var txt = "";
+switch (leMois) {
+  case 1:
+    txt = "Envoyer les voeux.";
+    break;
+  case 2:
+    txt = "Préparer WE au ski.";
+    break;
+  case 3:
+    txt = "Ménage de printemps.";
+    break;
+  case 4:
+    txt = "Invitation anniversaire le 16.";
+    break;
+  case 5:
+    txt = "Révision avant examen.";
+    break;
+  case 6:
+    txt = "Examen...";
+    break;
+  case 7:
+  case 8:
+    txt = "Les Vacances...";
+    break;
+  case 9:
+    txt = "Faire les vendanges";
+    break;
+  case 10:
+    txt = "Préparer la rentrée";
+    break;
+  case 11:
+    txt = "Vérifier déguisement Halloween";
+    break;
+  case 12:
+    txt = "Ne pas acheter les cadeaux au dernier moment.";
+    break;
+  default:
+    txt = "Erreur sur le mois.";
+}
+// alert("Pour le mois de " + lesMois_arr[leMois] + " penser à : " + txt);
+
+var art = 5;
+var artPlus = 5.6;
+var tot = art + art * 0.056;
+console.log(tot);
+var art2 = 2.6;
+var artMinus = 2.2;
+var tot2 = art2 - art2 * 0.022;
+console.log(tot2);
+console.log(Math.round(61.213213, 2));
+
+var date = new Date();
+console.log(date.now());
+//-----------------------------
+//trash
+//------------------------
 // switch (key) {
 //   case value:
 
@@ -34,3 +107,13 @@ switch (test) {
 //   default:
 //     break;
 // }
+// var table = ["test1", "test2", "test3"];
+// console.log(table);
+// console.log(...table);
+// var table2 = { test1: "test2", test4: "test5" };
+// // table,table2;
+// console.log(table, table2);
+// const testTarget = document.getElementById("test");
+// console.log(testTarget.outerText);
+// var testClass = document.querySelectorAll(".tesclass");
+// console.log(testClass[0]);
